68bb5171535e2f297716beb61952b52ef1e90c89
Diaser8 <diaser1987@gmail.com>
2024-05-12 14:16:31 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students1= list (students) # преобразовал множество в список
+students1.sort()
+print(students1)
+
+students_results = {}      # создание словаря в котором будут итоговые рузультаты
+
+
+a= sum(grades[0]) / len(grades[0])
+b= sum(grades[1]) / len(grades[1])
+c= sum(grades[2]) / len(grades[2])
+d= sum(grades[3]) / len(grades[3])
+e= sum(grades[4]) / len(grades[4])
+
+students_results['AARON'] = a
+students_results['Bilbo'] = b
+students_results ['Johnny'] = c
+students_results['Steve'] = d
+students_results ['Khendrik'] = e
+print(students_results)
